5c26882264981278d69a789c4990ffe1ae9d8236
Lani1321 <lani.berry13@gmail.com>
2016-08-31 12:04:09 -0400

    add comments to make it easier for other people to understand what my code is doing.

--- lib/raleigh_events/event.rb
+++ lib/raleigh_events/event.rb
@@ -1,11 +1,11 @@
 class RaleighEvents::Event
   attr_accessor :title, :date, :location, :url
 
-
+  # Scrape get-offline and return events based on that data
   def self.upcoming
-    # scrape get-offline and return events based on that data
     self.scrape_events
   end
+
     def self.scrape_events
       events = []
       doc = Nokogiri::HTML(open("https://www.get-offline.com/raleigh/upcoming-events"))
@@ -14,7 +14,7 @@ class RaleighEvents::Event
         # instantiating a new Event object as it iterates through each web element
         new_event = self.new  
         new_event.title = event.css("#template-name-link").text.strip
-        
+
         location = event.css(".location-bar").text.gsub("â\u0080¢", "").strip
         location_array = location.split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/)
         location_array.pop
