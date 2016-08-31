d7349532c39034ee971216b7dc9f8b2c01590727
Lani1321 <lani.berry13@gmail.com>
2016-08-31 12:19:12 -0400

    add require 'nokogiri' and 'open uri' to open html site i am scraping and add require_relative './raleigh_events/event'

--- lib/raleigh_events.rb
+++ lib/raleigh_events.rb
@@ -1,5 +1,7 @@
-require "raleigh_events/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry' #take this out once I want to publlish this gem
 
-module RaleighEvents
-  # Your code goes here...
-end
+require_relative "./raleigh_events/version" 
+require_relative './raleigh_events/event'
+require_relative './raleigh_events/cli'
