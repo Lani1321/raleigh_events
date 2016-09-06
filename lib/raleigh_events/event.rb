421f07a9c13470b01016b968eb32d2bf0a6fb245
Lani1321 <lani.berry13@gmail.com>
2016-09-06 13:04:37 -0400

    refactored .scrape_events so code is easily readable

--- lib/raleigh_events/event.rb
+++ lib/raleigh_events/event.rb
@@ -13,14 +13,20 @@ class RaleighEvents::Event
       doc.css(".experience-thumb").each do |event|
         # instantiating a new Event object as it iterates through each web element
         new_event = self.new  
-        new_event.title = event.css("#template-name-link").text.strip
+        
         location = event.css(".location-bar").text.gsub("â\u0080¢", "").strip
+       
+        # Puts every string into an array and removed the last unwanted value
         location_array = location.split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/)
         location_array.pop
-        new_event.location = location_array.join(' ')
         date = event.css(".experience-extra-details").children.last.text 
-        new_event.date = date.gsub(/\n/, "").strip 
-        new_event.url =  "https://www.get-offline.com#{event.attribute('href')}"
+
+        # Setting the attributes of Event instance 
+        new_event.date     = date.gsub(/\n/, "").strip
+        new_event.title    = event.css("#template-name-link").text.strip
+        new_event.url      =  "https://www.get-offline.com#{event.attribute('href')}"
+        new_event.location = location_array.join(' ')
+
         events << new_event
       end
       events
